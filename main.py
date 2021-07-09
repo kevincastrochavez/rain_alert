e4d9fc3126433f7ec88568de1f205ae5d9fa64c8
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-07-09 16:53:36 -0600

    completed the rain alert application and changed personal api keys and information for placeholders

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
 import requests
+from twilio.rest import Client
 
-API_KEY = '90110c6c5296ba96a34270bcbcee731b'
-MY_LAT = 43.826069
-MY_LONG = -111.789528
+API_KEY = 'Your OpenWeather API key'
+MY_LAT = 43.826069 # Yours
+MY_LONG = -111.789528 # Yours
 OWN_ENDPOINT = 'https://api.openweathermap.org/data/2.5/onecall'
+ACCOUNT_SID = 'Your Twilio account sid'
+AUTH_TOKEN = 'Your Twilio auth token'
 
 weather_params = {
     'lat': MY_LAT,
     'lon': MY_LONG,
-    'appid': API_KEY
+    'appid': API_KEY,
+    'exclude': 'current,minutely,daily'
 }
 
 response = requests.get(OWN_ENDPOINT, params=weather_params)
 response.raise_for_status()
+weather_data = response.json()
+weather_slice = weather_data['hourly'][:12] 
 
-data = response.json()
-print(data)
+will_rain = False
+
+for hour_data in weather_slice:
+    condition_code = hour_data['weather'][0]['id']
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    client = Client(ACCOUNT_SID, AUTH_TOKEN)
+    message = client.messages \
+                .create(
+                body="It's going to rain or snow today. Remember to bring an unmbrella",
+                from_='Your Twilio phone number',
+                to='Your actual phone number'
+                 )
+    print(message.status)
